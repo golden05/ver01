338803b91d40dda95cf03bd82c5fd55b84ad69ff
golden05 <golden05@gmail.com>
2023-04-17 21:39:42 +0800

    unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import Unocss from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Unocss(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
